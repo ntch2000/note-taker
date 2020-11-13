b76c56650a6563e5484b87fe3aa5e6a289ff067c
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-11-12 20:59:24 -0500

    Updated server with middleware code

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,18 @@
+// 1. require express package
 const express = require("express");
 const path = require("path");
 
+// 2. create instance of express
 const app = express();
+
+// 3. set listen PORT
 const PORT = process.env.PORT || 3000;
 
+// 5. add middleware code
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// 4. listen on  PORT
 app.listen(PORT, () => {
   console.log(`App listening on PORT ${PORT}`);
 });
