a518b4fca096a48a92d26c546a40a134d41296af
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-11-12 21:08:50 -0500

    Added main routes (/api/notes, /notes, main)

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const path = require("path");
 
+const fs = require("fs");
+
 // 2. create instance of express
 const app = express();
 
@@ -15,4 +17,29 @@ app.use(express.json());
 // 4. listen on  PORT
 app.listen(PORT, () => {
   console.log(`App listening on PORT ${PORT}`);
+});
+
+// set object array for notes
+const notes = [
+  {
+    title: "Note Title",
+    text: "Note text",
+  },
+];
+
+// setup routes code
+
+// notes route
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// main page route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// api route
+app.get("/api/notes", (req, res) => {
+  return res.json(notes);
 });
